cc9bce961cff289396edd518e4b7414f860d5c75
mssavai <mssavai@gmail.com>
2016-02-22 19:01:45 +0300

    Add license info

--- api/src/main/java/org/openmrs/module/expandedcohort/api/impl/CohortDefinitionDataServiceImpl.java
+++ api/src/main/java/org/openmrs/module/expandedcohort/api/impl/CohortDefinitionDataServiceImpl.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.expandedcohort.api.impl;
 
 
@@ -12,9 +25,6 @@ import org.openmrs.module.expandedcohort.api.model.CohortDefinitionData;
 
 import java.util.List;
 
-/**
- * Created by savai on 3/10/15.
- */
 public class CohortDefinitionDataServiceImpl extends BaseOpenmrsService implements CohortDefinitionDataService {
     private final Log log = LogFactory.getLog(this.getClass());
 
--- omod/src/main/java/org/openmrs/module/expandedcohort/web/utils/WebConverter.java
+++ omod/src/main/java/org/openmrs/module/expandedcohort/web/utils/WebConverter.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.expandedcohort.web.utils;
 
 import org.openmrs.Cohort;
@@ -7,9 +20,6 @@ import org.openmrs.module.expandedcohort.api.model.CohortDefinitionData;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by savai on 3/23/15.
- */
 public class WebConverter {
     public static Map<String, Object> convertCohortDefinitionData(final CohortDefinitionData cohortDefinitionData){
         Map<String, Object> map = new HashMap<String, Object>();
